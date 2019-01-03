fb4db93c90807a94af7bcef1b7b897179261bbb6
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-01-03 19:19:38 +1100

    remove post app

--- client/src/router.js
+++ client/src/router.js
@@ -1,34 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Playground from "./views/Playground.vue";
-import Posts from "./views/Posts.vue";
-import AddPost from "./views/AddPost.vue";
-import EditPost from "./views/EditPost.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  // mode: "history",
+  mode: "history",
   routes: [
     {
       path: "/",
       name: "playground",
       component: Playground
-    },
-    {
-      path: "/posts",
-      name: "posts",
-      component: Posts
-    },
-    {
-      path: "/posts/add",
-      name: "addPost",
-      component: AddPost
-    },
-    {
-      path: "/posts/:id/edit",
-      name: "editPost",
-      component: EditPost
-    },
+    }
   ]
 });
