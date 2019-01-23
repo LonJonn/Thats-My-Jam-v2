5ff0186c06ff4088dfac412f03567fc0edb014b3
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-01-23 18:20:14 +1100

    custom styling

--- client/src/App.vue
+++ client/src/App.vue
@@ -29,61 +29,7 @@ export default {
 <style lang="scss">
 @import "~bulma/sass/utilities/_all";
 
-// Set your brand colors
-$purple: #8a4d76;
-$pink: #fa7c91;
-$brown: #757763;
-$beige-light: #d0d1cd;
-$beige-lighter: #eff0eb;
-
-// Update Bulma's global variables
-$grey-dark: $brown;
-$grey-light: $beige-light;
-$primary: $purple;
-$link: $pink;
-
-// Update some of Bulma's component variables
-$body-background-color: $beige-lighter;
-$control-border-width: 2px;
-
-$colors: (
-  "white": (
-    $white,
-    $black
-  ),
-  "black": (
-    $black,
-    $white
-  ),
-  "light": (
-    $light,
-    $light-invert
-  ),
-  "dark": (
-    $dark,
-    $dark-invert
-  ),
-  "primary": (
-    $primary,
-    $primary-invert
-  ),
-  "info": (
-    $info,
-    $info-invert
-  ),
-  "success": (
-    $success,
-    $success-invert
-  ),
-  "warning": (
-    $warning,
-    $warning-invert
-  ),
-  "danger": (
-    $danger,
-    $danger-invert
-  )
-);
+//custom styling
 
 @import "~bulma";
 @import "~buefy/src/scss/buefy";
