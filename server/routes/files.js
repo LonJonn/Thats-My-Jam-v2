0a44a4c570dd5ab78a9853eddd2d294f20c20afa
Leon Salsiccia <leon.salsiccia@gmail.com>
2019-01-26 11:41:47 +1100

    removed redundant function

--- server/routes/files.js
+++ server/routes/files.js
@@ -4,25 +4,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const response = await readDir(path.join(__dirname, "../static/files"));
-  res.send(response);
+  fs.readdir(path.join(__dirname, "../static/files"), (err, files) => {
+    res.send(files.filter(file => !file.startsWith(".")));
+  });
 });
 
 router.delete("/:file", (req, res) => {
   try {
     fs.unlinkSync(path.join(__dirname, "../static/files/" + req.params.file));
-    res.send("File deleted.");
   } catch (error) {
     res.status(404).send("File not found!\nNo file deleted.");
   }
-});
 
-function readDir(dir) {
-  return new Promise(resolve => {
-    fs.readdir(dir, (error, files) => {
-      resolve(files.filter(file => !file.startsWith(".")));
-    });
-  });
-}
+  res.send("File deleted.");
+});
 
 module.exports = router;
